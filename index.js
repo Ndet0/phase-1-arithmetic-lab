a7248d525f0dd4d97458fbe94b48d8e2e49e5798
Ndet0 <festusndet00@gmail.com>
2025-09-13 05:43:26 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,86 @@
 
-// Write your code here
+// Write your code
+
+var num1=31;
+var num2=2;
+var multiply= num1 * num2;
+console.log(multiply)
+
+var random = Math.floor(Math.random() * 100) + 1;
+ console.log(random)
+
+ var num3=16;
+ var num4=6;
+ var mod= num3 % num4;
+
+ console.log(mod)
+
+ var max=Math.max(5,20,10,15);
+ console.log(max)
+//let counter = 0;
+//=> undefined
+
+// Return the current value of 'counter' and then decrement by 1
+//counter--;
+//=> 0
+
+// Check the new value of 'counter'
+//counter;
+//=> -1
+
+// Decrement 'counter' and then return the new value
+//--counter;
+//=> -2
+
+// Check the new value of 'counter'
+//counter;
+//=> -2
+
+//let counter = 0;
+//=> undefined
+
+//counter++;
+//=> 0
+
+//counter;
+//=> 1
+
+
+//++counter;
+//=> 1
+
+//counter;
+//=> 1
+
+//let counter = 0;
+//=> undefined
+
+//counter += 10;
+//=> 10
+
+//counter -= 2;
+//=> 8
+
+//counter *= 4;
+//=> 32
+
+//counter /= 2;
+//=> 16
+
+//counter %= 6;
+//=> 4
+
+//counter **= 3;
+//=> 64
+
+//counter++;
+//=> NaN
+
+// console.log(counter)
+
+//Number.isInteger(42);
+//=> true
+
+//Number.isInteger(0.42);
+//=> false
+
